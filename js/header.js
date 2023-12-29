30d4976ef4de00645406615cdd362b72852251d3
tufanmulla <iamtufanmulla@gmail.com>
2023-12-30 00:22:04 +0530

    add a features in header navigation bar which is sub category box and add a js features in between search bar and navigation bar

---
+++ js/header.js
@@ -0,0 +1,34 @@
+// When User Click On The #searchIcon ID Then The #navigationBar Hide And #search Bar Shown
+let navigationBar = document.querySelector("#navigationBar");
+let headerPart3 = document.querySelector("#headerPart3");
+let search = document.querySelector("#search");
+let searchIcon = document.querySelector("#searchIcon");
+
+searchIcon.addEventListener("click", (event) => {
+  event.stopPropagation();
+  if (search.classList.contains("hidden")) {
+    search.classList.remove("hidden");
+    navigationBar.classList.add("hidden");
+    headerPart3.classList.remove("col-start-9");
+    headerPart3.classList.add("col-start-6");
+  } else {
+    search.classList.add("hidden");
+    navigationBar.classList.remove("hidden");
+    headerPart3.classList.remove("col-start-6");
+    headerPart3.classList.add("col-start-9");
+  }
+});
+
+document.addEventListener("click", (event) => {
+  if (
+    !event.target.closest("#searhIcon") &&
+    !event.target.closest("#search") &&
+    navigationBar.classList.contains("hidden") &&
+    !search.classList.contains("hidden")
+  ) {
+    search.classList.add("hidden");
+    navigationBar.classList.remove("hidden");
+    headerPart3.classList.add("col-start-9");
+    headerPart3.classList.remove("col-start-6");
+  }
+});
