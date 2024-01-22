aefa9394ebe48ccaa38509f3fc80b15cff379183
tufanmulla <iamtufanmulla@gmail.com>
2024-01-23 00:10:41 +0530

    re design for mobile first responsiveness

--- js/index.js
+++ js/index.js
@@ -1,29 +1,76 @@
-// When User Click On The #searchIcon ID Then The #navigationBar Hide And #search Bar Shown
+// define a variable
+
+let menuBar = document.querySelector("#menuBar");
 let navigationBar = document.querySelector("#navigationBar");
 let headerPart3 = document.querySelector("#headerPart3");
 let search = document.querySelector("#search");
 let searchIcon = document.querySelector("#searchIcon");
 
-searchIcon.addEventListener("click", (event) => {
-  event.stopPropagation();
-  if (search.classList.contains("hidden")) {
-    search.classList.remove("hidden");
-    search.focus();
-    navigationBar.classList.add("hidden");
+// when click on the #menuBar id then toggling the #navigationBar
+menuBar.addEventListener("click", (event) => {
+  if (navigationBar.classList.contains("scale-y-0")) {
+    navigationBar.classList.remove("scale-y-0");
+  } else {
+    navigationBar.classList.add("scale-y-0");
+  }
+});
+//when click on the (document) then add class scale-y-0 of the #navigationBar
+document.addEventListener("click", (menuBarEvent) => {
+  if (
+    !menuBarEvent.target.closest("#menuBar") &&
+    !menuBarEvent.target.closest("#navigationBar") &&
+    !navigationBar.classList.contains("scale-y-0")
+  ) {
+    navigationBar.classList.add("scale-y-0");
   }
 });
 
-document.addEventListener("click", (event) => {
-  if (
-    !event.target.closest("#searhIcon") &&
-    !event.target.closest("#search") &&
-    navigationBar.classList.contains("hidden") &&
-    !search.classList.contains("hidden")
-  ) {
-    search.classList.add("hidden");
-    navigationBar.classList.remove("hidden");
-  }
+//when click #hierarchy-li then show the hieerarchy-Li-Div
+let hierarchyLi = document.querySelectorAll("#hierarchy-li");
+let hierarchyLiDiv = document.querySelectorAll("#hierarchy-li-div");
+let hierarchyImg = document.querySelectorAll("#hierarchy-img");
+hierarchyLi.forEach((li, liIndex) => {
+  li.addEventListener("click", () => {
+    hierarchyLiDiv.forEach((liDiv, liDivIndex) => {
+      if (liIndex === liDivIndex) {
+        liDiv.classList.remove("hidden");
+        liDiv.classList.add("block");
+      } else {
+        liDiv.classList.add("hidden");
+        liDiv.classList.remove("block");
+      }
+    });
+    hierarchyImg.forEach((img, imgIndex) => {
+      if (liIndex === imgIndex) {
+        img.classList.add("rotate-180");
+      } else {
+        img.classList.remove("rotate-180");
+      }
+    });
+  });
 });
+
+// When User Click On The #searchIcon ID Then The #navigationBar Hide And #search Bar Shown
+// searchIcon.addEventListener("click", (event) => {
+
+//   if (search.classList.contains("hidden")) {
+//     search.classList.remove("hidden");
+//     search.focus();
+//     navigationBar.classList.add("hidden");
+//   }
+// });
+
+// document.addEventListener("click", (event) => {
+//   if (
+//     !event.target.closest("#searhIcon") &&
+//     !event.target.closest("#search") &&
+//     navigationBar.classList.contains("hidden") &&
+//     !search.classList.contains("hidden")
+//   ) {
+//     search.classList.add("hidden");
+//     navigationBar.classList.remove("hidden");
+//   }
+// });
 
 // Section Three JavaScript
 let layers = document.querySelectorAll("#layer");
